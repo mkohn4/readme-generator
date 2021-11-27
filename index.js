24b98aced6ba8c68ea77b5944f7dad1d78f59514
maxkohn <kohn.max@gmail.com>
2021-11-27 13:16:22 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,135 @@
+const inquirer = require ('inquirer');
+const {writeFile, markup, licenseInfo} = require('./templates/templates.js');
+const fs = require('fs');
+
+const readmeInfo = () => {
+        //if readmeData.projects doesnt, exist, create it
+        /*if (readmeData.projects === undefined) {
+            readmeData.projects = [];
+        }
+        console.log(`Add a New Project README`);*/
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (Required)',
+            validate: title => {
+                if (title) {
+                    return true;
+                } else {
+                    console.log('Enter your Project Name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is the description of your project? (Required)',
+            validate: description => {
+                if (description) {
+                    return true;
+                } else {
+                    console.log('Enter your Project Description!');
+                    return false;
+                }
+            }
+        },{
+            type: 'input',
+            name: 'instructions',
+            message: 'What are the installation instructions? (Required)',
+            validate: instructions => {
+                if (instructions) {
+                    return true;
+                } else {
+                    console.log('Enter your Projects Installaton Instructions!');
+                    return false;
+                }
+            }
+        },{
+            type: 'input',
+            name: 'usageInfo',
+            message: 'What is the Usage Information for this Project?'
+        },{
+            type: 'input',
+            name: 'contributionGuidelines',
+            message: 'What are the contribution guidelines for your project?'
+        },{
+            type: 'input',
+            name: 'testInstructions',
+            message: 'What are the test instructions for your project?'
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'Which license would you like to use?',
+            choices: ['MIT', 'GNU'],
+            validate: license => {
+                if (license) {
+                    return true;
+                } else {
+                    console.log('Choose a License!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'badges',
+            message: 'Would you like to add a custom badge color for the License?',
+            default: false
+        },
+        {
+            type: 'input',
+            name: 'badgeColor',
+            message: 'What would you like your Badge Color to be?',
+            when: ({badges}) => {
+                //if user selected true in badges prompt, show this q
+                if (badges) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your Github Username?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?'
+        }
+    ])
+    .then(readmeAnswers => {
+        console.log(readmeAnswers);
+        /*console.log(readmeData);
+        readmeData.projects = [];
+        //push responses to the readmeData.projects array
+        readmeData.projects.push(readmeAnswers);*/
+        //return the current data
+        return readmeAnswers;
+    })
+};
+
+
+
+readmeInfo()
+    .then(readmeAnswers => {
+         return licenseInfo(readmeAnswers);
+    }).then(readmeAnswers => {
+        return markup(readmeAnswers);
+    }).then(markup => {
+        return writeFile(markup);
+    });
+
+/*function init () {
+    prompt(questions)
+    .then(generateMarkup)
+    .then((templateToSave))
+}
+function generateMarkup(answers) {
+    return markup(answers);
+}*/
---
+++ readmes/readme.md
@@ -0,0 +1,61 @@
+
+# Title
+
+## Table of Contents
+[Description][##description]
+[Installation Instructions][##installation]
+[Usage Details][##usage]
+[Contribution Guidelines][##contribution-guidelines]
+[Testing Instructions][##testing-instructions]
+[Questions?][##questions?]
+[License Info][##license]
+
+
+## Description
+
+Description
+
+## Installation
+
+Installation
+
+## Usage
+
+Usage Info
+
+## Contribution Guidelines
+
+Contribution Guidelines
+
+## Testing Instructions
+
+Test Instructions
+
+## Questions?
+
+Check out my Github: http://www.github.com/mkohn4
+Email Me: kohn.max@gmail.com
+
+## License
+MIT License
+
+        Copyright (c) 2021 mkohn4
+        
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+        
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+        
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+
